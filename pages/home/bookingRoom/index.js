df16f31efa46b0f423d81e8b996e205023b48c24
Jason <mac@MacdeMacBook-Air-118.local>
2019-01-13 20:54:08 +0800

    modfy

--- pages/home/bookingRoom/index.js
+++ pages/home/bookingRoom/index.js
@@ -1,11 +1,24 @@
 // pages/home/bookingRoom/index.js
-Page({
+const api = require('../../../utils/api.js');
+const util = require('../../../utils/util.js');
+import WxValidate from "../../../plugins/wx-validate/WxValidate.js";
+const app = getApp();
+const apiUrl = {
+    bindMobileUrl: 'sms/bindmobilepost',
+    setMobileUrl: 'user/setmobilepost'
+};
 
+Page({
     /**
      * 页面的初始数据
      */
     data: {
-
+        bind_mobile: {
+            mobile: '',
+            code: '',
+            code_flag: false, //验证码
+            count_down: 60, //验证码倒计时
+        },
     },
 
     /**
@@ -15,32 +28,117 @@ Page({
 
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+    //绑定手机号
+    bindMobile: function (e) {
+        let self = this;
 
+        self.setData({
+            ['bind_mobile.mobile']: e.detail.value
+        })
     },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
+    //绑定验证码
+    bindCode: function (e) {
+        let self = this;
 
+        self.setData({
+            ['bind_mobile.code']: e.detail.value
+        })
     },
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
+    //获取验证码
+    getCode: function () {
+        let self = this;
+        let bind_mobile = self.data.bind_mobile;
 
+        // 参数规则
+        const rules = {
+            mobile: {
+                required: true,
+                tel: true,
+                number: true
+            }
+        };
+
+        const messages = {
+            mobile: {
+                required: '请输入您的联系电话',
+                tel: '请输入正确的手机号',
+                number: '验证码必须为数字',
+            }
+        };
+        let validate = new WxValidate(rules, messages);
+        const checkData = {
+            detail: {
+                value: {}
+            }
+        };
+        checkData.detail.value = {
+            mobile: bind_mobile.mobile,
+        };
+
+        // 参数校验
+        if (!validate.checkForm(checkData)) {
+            const error = validate.errorList[0];
+            wx.showToast({
+                title: error.msg,
+                icon: 'none',
+                duration: 2000
+            })
+            return;
+        }
+
+        // 倒计时
+        self.setCodeInterval();
+
+        // 申请验证码
+        let postData = {
+            mobile: bind_mobile.mobile
+        };
+        api.doHttp(apiUrl.bindMobileUrl, postData).then(res => {
+            wx.showToast({
+                title: res.msg,
+                icon: "none",
+                duration: 2000
+            })
+        });
     },
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
+    //倒计时
+    setCodeInterval: function () {
+        let self = this;
 
+        self.setData({
+            ['bind_mobile.code_flag']: true
+        });
+        let interval = setInterval(() => {
+            let time = self.data.bind_mobile.count_down;
+            self.setData({
+                ['bind_mobile.count_down']: time - 1
+            });
+            if (self.data.bind_mobile.count_down == 0) {
+                clearInterval(interval);
+                self.setData({
+                    ['bind_mobile.code_flag']: false,
+                    ['bind_mobile.count_down']: 60
+                });
+            }
+        }, 1000);
+    },
+
+    //绑定手机
+    setMobile: function() {
+        let self = this;
+        let bind_mobile = self.data.bind_mobile;
+        let postData = {
+            mobile: bind_mobile.mobile,
+            code: bind_mobile.code
+        };
+
+        api.doHttp(api.setMobileUrl, postData).then(res => {
+            let data = res.data;
+
+        });
     },
 
     /**
