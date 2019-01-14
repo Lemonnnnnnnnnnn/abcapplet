759de383fc83d7d6776f685e047569d363a2ca44
dcq <dcq@kuryun.cn>
2019-01-14 10:05:38 +0800

    modify

--- pages/home/bookingRoom/index.js
+++ pages/home/bookingRoom/index.js
@@ -17,7 +17,7 @@ Page({
      */
     data: {
         id: 1, //公寓ID
-        show_mobile_box: 0, 
+        show_mobile_box: 0,
         bind_mobile: {
             mobile: '',
             code: '',
@@ -54,13 +54,32 @@ Page({
             title: [],
             index: 0,
             cbd_id: 0
-        }
+        },
+        order_time: {
+            list: [
+                ['2019年', '2020年', '2021年', '2022年', '2023年'],
+                ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+                [],
+                ['10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00']
+            ],
+            date_time_str: '请选择',
+            date_time: '',
+            index: 0
+        },
+        living_time: {
+            list: [{id: 1, title: '马上'}, {id: 7, title: '7天'}, {id: 15, title: '15天'}, {id: 32, title: '一个月后'}],
+            id: 1
+        },
+        living_num: {
+            list: [{ id: 1, title: '1人' }, { id: 2, title: '2人' }, { id: 3, title: '3人' }, { id: 4, title: '3人以上' }],
+            id: 1
+        },
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function (options) {
+    onLoad: function(options) {
         let self = this;
 
         if (!wx.getStorageSync('user_info')) {
@@ -74,7 +93,7 @@ Page({
             self.initData();
         }
 
-        if(options.id) {
+        if (options.id) {
             self.setData({
                 id: options.id
             });
@@ -91,10 +110,12 @@ Page({
         self.getSimple();
         //获取字典数据
         self.listDict();
+        //获取可选时间数据
+        self.getDateTime();
     },
 
     //获取用户信息
-    getUser: function () {
+    getUser: function() {
         let self = this;
         let postData = {
 
@@ -104,9 +125,9 @@ Page({
             let data = res.data;
             let user = data.user;
             let show_mobile_box = 0;
-            
+
             //电话号码为空，默认显示绑定手机弹框
-            if(!user.mobile) {
+            if (!user.mobile) {
                 show_mobile_box = 1
             }
 
@@ -140,7 +161,7 @@ Page({
     },
 
     //绑定手机号
-    bindMobile: function (e) {
+    bindMobile: function(e) {
         let self = this;
 
         self.setData({
@@ -149,7 +170,7 @@ Page({
     },
 
     //绑定验证码
-    bindCode: function (e) {
+    bindCode: function(e) {
         let self = this;
 
         self.setData({
@@ -158,7 +179,7 @@ Page({
     },
 
     //获取验证码
-    getCode: function () {
+    getCode: function() {
         let self = this;
         let bind_mobile = self.data.bind_mobile;
 
@@ -215,7 +236,7 @@ Page({
     },
 
     //倒计时
-    setCodeInterval: function () {
+    setCodeInterval: function() {
         let self = this;
 
         self.setData({
@@ -296,14 +317,14 @@ Page({
 
             self.setData({
                 ['user_info.mobile']: data.user.mobile
-            }); 
+            });
             //关闭手机绑定弹框
-            self.closeMobilBox();   
+            self.closeMobilBox();
         });
     },
 
     //获取公寓简单信息
-    getSimple: function () {
+    getSimple: function() {
         let self = this;
         let postData = {
             id: self.data.id
@@ -327,7 +348,7 @@ Page({
     },
 
     //公寓严选字典数据列表
-    listDict: function () {
+    listDict: function() {
         let self = this;
         let postData = {
             city: wx.getStorageSync('city_id')
@@ -335,52 +356,46 @@ Page({
 
         api.doHttp(apiUrl.listDictUrl, postData).then(res => {
             let data = res.data;
-            let cbd_list = [];
-            let cbd_title = [];
-            let cbd_id = 0;
-            let price_title = [];
-            let price_id = 0;
-            let house_type_title = [];
-            let house_type_id = 0;
+            let cbd_list = [{
+                id: 0,
+                title: '全部'
+            }];
+            let cbd_title = ['全部'];
+            let price_title = ['全部'];
+            let house_type_title = ['全部'];
 
             //cbd格式处理
-            if(data.cbd_list.length) {
+            if (data.cbd_list.length) {
                 data.cbd_list.forEach((el, index) => {
                     el.cbd.forEach((el, index) => {
-                        cbd_list.push({ id: el.id, title: el.title });
+                        cbd_list.push({
+                            id: el.id,
+                            title: el.title
+                        });
                         cbd_title.push(el.title);
                     });
                 });
-                //初始cbd值
-                cbd_id = data.cbd_list[0].id;
             }
             //价格格式处理
-            if(data.price_list.length) {
+            if (data.price_list.length) {
                 data.price_list.forEach((el, index) => {
                     price_title.push(el.title);
                 });
-                //初始价格值
-                price_id = data.price_list[0].id
             }
             //户型格式处理
-            if(data.housetype_list.length) {
+            if (data.housetype_list.length) {
                 data.housetype_list.forEach((el, index) => {
                     house_type_title.push(el.title);
                 });
-                //初始户型值
-                house_type_id = data.housetype_list[0].id;
             }
 
             self.setData({
                 ['cbd.list']: cbd_list,
                 ['cbd.title']: cbd_title,
-                ['cbd.cbd_id']: cbd_id,
                 ['price.list']: data.price_list,
                 ['price.title']: price_title,
-                ['price.price_id']: price_id,
                 ['house_type.list']: data.housetype_list,
-                ['house_type.title']: house_type_title,
-                ['house_type.house_type_id']: house_type_id
+                ['house_type.title']: house_type_title
             });
         });
     },
@@ -391,14 +406,14 @@ Page({
         let house_type = self.data.house_type;
         let index = e.detail.value;
         let house_type_id = house_type.house_type_id;
-        
+
         //筛选id
         house_type.list.forEach((el, index) => {
-            if(el.title == house_type.title[index]) {
+            if (el.title == house_type.title[index]) {
                 house_type_id = el.id;
             }
         });
-        
+
         self.setData({
             ['house_type.house_type_id']: house_type_id,
             ['house_type.index']: index
@@ -414,7 +429,7 @@ Page({
 
         //筛选id
         price.list.forEach((el, index) => {
-            if(el.title == price.title[index]) {
+            if (el.title == price.title[index]) {
                 price_id = el.id;
             }
         });
@@ -434,7 +449,7 @@ Page({
 
         //筛选id
         cbd.list.forEach((el, index) => {
-            if(el.title == cbd.title[index]) {
+            if (el.title == cbd.title[index]) {
                 cbd_id = el.id;
             }
         });
@@ -445,24 +460,196 @@ Page({
         });
     },
 
+    //改变看房时间
+    orderTimeChange: function (e) {
+        let self = this;
+        let order_time = self.data.order_time;
+        let value = e.detail.value;
+        let year = order_time.list[0][value[0]];
+        let month = order_time.list[1][value[1]];
+        let date = order_time.list[2][value[2]];
+        let time = order_time.list[3][value[3]] + ':00';
+        let date_time_str = year + month + date + ' ' + time;
+        let date_time = date_time_str.replace('年', '-').replace('月', '-').replace('日', '')
+
+        if ((new Date(date_time.replace(/\-/g, "/")).getTime()) < (new Date().getTime())) {
+            wx.showToast({
+                title: '无效时间',
+                duration: 2000
+            })
+        } else {
+            self.setData({
+                ['order_time.index']: value,
+                ['order_time.date_time']: date_time,
+                ['order_time.date_time_str']: date_time_str
+            })
+        }
+    },
+
+    //切换时间
+    orderTimeColumnChange: function (e) {
+        let self = this;
+        let order_time = self.data.order_time;
+        let obj = e.detail;
+        let mChange;
+        let yChange;
+
+        if(obj.column == 0) {
+            if (obj.value == 0) {
+                yChange = false;
+                self.getDateTime();
+            } else {
+                yChange = true;
+                let m = new Date().getMonth() + 1;
+                let arrM = ['1月'];
+                if (m == 12) {
+                    arrM.push('2月')
+                }
+                let arrD = [];
+                for (let i = 1; i <= 31; i++) {
+                    arrD.push(i + '日')
+                }
+                let arrH = [];
+                for (let j = 10; j <= 21; j++) {
+                    arrH.push(j + ':00')
+                }
+                self.setData({
+                    ['order_time.list']: [order_time.list[0], arrM, arrD, arrH],
+                })
+            }
+        } else if (obj.column == 1) {
+            if (obj.value == 0) {
+                if (!yChange) {
+                    mChange = false;
+                    self.getDateTime();
+                } else {
+                    mChange = true;
+                }
+            } else {
+                mChange = true;
+                let m = new Date().getMonth() + 1;
+                let m1 = m + obj.value;
+                let maxD = (m1 == 2) ? 28 : (m1 == 4 || m1 == 6 || m1 == 9 || m1 == 11) ? 30 : 31;
+                let arrD = [];
+                for (let i = 1; i <= maxD; i++) {
+                    arrD.push(i + '日')
+                }
+                let arrH = [];
+                for (let j = 10; j <= 21; j++) {
+                    arrH.push(j + ':00')
+                }
+                self.setData({
+                    ['order_time.list']: [order_time.list[0], order_time.list[1], arrD, arrH],
+                })
+            }
+        } else if (obj.column == 2) {
+            if (obj.value == 0) {
+                if (mChange == false) {
+                    self.getDateTime();
+                }
+            } else {
+                let arrH = [];
+                for (let j = 10; j <= 21; j++) {
+                    arrH.push(j + ':00')
+                }
+                self.setData({
+                    ['order_time.list']: [order_time.list[0], order_time.list[1], order_time.list[2], arrH],
+                })
+            }
+        }
+    },
+
+    //获取可选时间数据
+    getDateTime: function() {
+        let self = this;
+        let y = new Date().getFullYear();
+        let m = new Date().getMonth() + 1;
+        let d = new Date().getDate();
+        let h = new Date().getHours();
+
+        let arrY = (m + 3) > 12 ? [y + '年', y + 1 + '年'] : [y + '年'];
+
+        let minH = h;
+        let minD = d;
+        let maxD = (m == 2) ? 28 : (m == 4 || m == 6 || m == 9 || m == 11) ? 30 : 31;
+        let minM = m;
+
+        if(h < 9) {
+            minH = 10;
+        }else if (h > 19) {
+            minH = 10;
+            minD++;
+        }else if (h <= 19) {
+            //minH++;
+            minH = parseInt(minH) + parseInt(2) //看房时间推后2小时
+        }else {
+            minH = 10;
+            if(d < maxD) {
+                minD++;
+            }else {
+                minD = 1;
+                maxD = (m + 1 == 2) ? 28 : (m + 1 == 4 || m + 1 == 6 || m + 1 == 9 || m + 1 == 11) ? 30 : 31;
+                minM == 12 ? (minM = 1) : minM++
+            }
+        }
+        let arrH = [];
+        for(let i = minH; i <= 21; i++) {
+            arrH.push(i + ':00')
+        }
+        let arrD = [];
+        for (let j = minD; j <= maxD; j++) {
+            arrD.push(j + '日')
+        }
+        let arrM = [];
+        for (let k = minM; k <= (minM + 2); k++) {
+            if (k <= 12) {
+                arrM.push(k + '月')
+            }
+        }
+
+        self.setData({
+            ['order_time.list']: [arrY, arrM, arrD, arrH]
+        });
+    },
+
+    //选择入住时间
+    livingTimeSelected: function(e) {
+        let self = this;
+        let dataset = e.currentTarget.dataset;
+        
+        self.setData({
+            ['living_time.id']: dataset.id
+        });
+    },
+
+    //选择入住人数
+    livingNumSelected: function (e) {
+        let self = this;
+        let dataset = e.currentTarget.dataset;
+
+        self.setData({
+            ['living_num.id']: dataset.id
+        });
+    },
+
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
-    onPullDownRefresh: function () {
+    onPullDownRefresh: function() {
 
     },
 
     /**
      * 页面上拉触底事件的处理函数
      */
-    onReachBottom: function () {
+    onReachBottom: function() {
 
     },
 
     /**
      * 用户点击右上角分享
      */
-    onShareAppMessage: function () {
+    onShareAppMessage: function() {
 
     }
 })
