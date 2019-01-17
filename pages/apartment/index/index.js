529615a8bcb6e801d71713b675e7106cd1e334f8
dcq <dcq@kuryun.cn>
2019-01-17 14:52:03 +0800

    modify

--- pages/apartment/index/index.js
+++ pages/apartment/index/index.js
@@ -285,7 +285,11 @@ Page({
      * 页面相关事件处理函数--监听用户下拉动作
      */
     onPullDownRefresh: function () {
+        let self = this;
 
+        self.initData();
+        wx.stopPullDownRefresh();
+        wx.hideLoading();
     },
 
     /**
