b44f09771d16b06cf7c460e886a803f70d8450ff
dcq <dcq@kuryun.cn>
2019-01-23 19:49:06 +0800

    modify

--- pages/apartment/commuteHouse/index.js
+++ pages/apartment/commuteHouse/index.js
@@ -1,46 +1,182 @@
 // pages/apartment/commuteHouse/index.js
+const api = require('../../../utils/api.js');
+const util = require('../../../utils/util.js');
+const QQMapWX = require('../../../utils/qqmap-wx-jssdk.js');
+const app = getApp();
+const apiUrl = {
+    commuteUrl: 'apartment/commutepost',
+};
+
+// 实例化API核心类
+let qqmapsdk = new QQMapWX({
+    key: 'NDSBZ-P3MKK-V5RJH-AJ5YD-WVAEF-GEFJM'
+});  
+
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        location: {
+            latitude: '',
+            longitude: '',
+            address: '',
+        },
+        time: {
+            title: ['10分钟', '20分钟', '30分钟', '60分钟', '90分钟'],
+            list: [{
+                id: 10,
+                title: '10分钟'
+            }, {
+                id: 20,
+                title: '20分钟'
+            }, {
+                id: 30,
+                title: '30分钟'
+            }, {
+                id: 60,
+                title: '60分钟',
+            }, {
+                id: 90,
+                title: '90分钟'
+            }],
+            index: 0,
+            id: 10
+        },
+        vehicle: 'bus',   //交通工具
+        apartment: {
+            current_page: 1,
+            page_size: 5,
+            total: 0,
+            list: [],
+        },
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
+        let self = this;
 
+        //数据初始化
+        self.initData();
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+    //数据初始化
+    initData: function() {
+        let self = this;
 
+        //获取用户所在地址
+        self.getLocation();
     },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
+    //获取用户所在地址
+    getLocation: function() {
+        let self = this;
 
+        wx.getLocation({
+            type: 'gcj02',
+            success: res => {
+                self.setData({
+                    ['location.longitude']: res.longitude,
+                    ['location.latitude']: res.latitude
+                });
+                //通勤找房
+                self.commute();
+                //根据坐标获取当前位置名称，显示在顶部:腾讯地图逆地址解析
+                qqmapsdk.reverseGeocoder({
+                    location: {
+                        latitude: res.latitude,
+                        longitude: res.longitude
+                    },
+                    get_poi: 1, 
+                    success: res => {
+                        let address = res.result.formatted_addresses.recommend;
+            
+                        //当前位置信息
+                        self.setData({
+                            ['location.address']: address
+                        });
+                        console.log(self.data.location.address);
+                    }
+                });
+            }
+        })
     },
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
+    //改变行程时间
+    timeChange: function(e) {
+        let self = this;
+        let time = self.data.time;
+        let title = time.title;
+        let list = time.list;
+        let id = 10;
 
+        //获取行程id
+        list.forEach((el, index) => {
+            if(el.title == title[e.detail.value]) {
+                id = el.id;
+            }
+        });
+
+        self.setData({
+            ['time.id']: id
+        });
     },
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
+    //交通工具选择
+    vehicleChange: function(e) {
+        let self = this;
+        let dataset = e.currentTarget.dataset;
+        console.log(dataset);
+        
+        self.setData({
+            vehicle: dataset.vehicle
+        });
+    },
 
+    //通勤找房
+    commute: function (type = 1) {
+        let self = this;
+
+        let postData = {
+            latitude: self.data.location.latitude,
+            longitude: self.data.location.longitude,
+            time: self.data.time.id,
+            vehicle: self.data.vehicle
+        };
+
+        api.doHttp(apiUrl.commuteUrl, postData).then(res => {
+            let data = res.data;
+            let list = data.list;
+            let total = data.total;
+            let apartment = self.data.apartment;
+            let format_list = [];
+
+            list.forEach((el, index) => {
+                //字符串转成数组，并截取前2个
+                if(el.tags) {
+                    el.tags = el.tags.split(',').slice(0, 2);
+                }
+                if (el.rules) {
+                    el.rules = el.rules.slice(0, 3);
+                }
+                format_list.push(el);
+            });
+
+            if (type == 1) {
+                //刷新清空公寓列表数组
+                self.setData({
+                    ['apartment.list']: [],
+                });
+            }
+
+            self.setData({
+                ['apartment.list[' + (apartment.current_page - 1) + ']']: format_list,
+                ['apartment.total']: total
+            });
+        });
     },
 
     /**
