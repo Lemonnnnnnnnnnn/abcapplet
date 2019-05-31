cf5870b153e9d633a5b764de0d30fec5145d7c29
zhangxiangliang <admin@pushme.top>
2019-05-31 17:16:05 +0800

    feat: (pages) 增加用户主页

--- src/pages/apartment/home.js
+++ src/pages/apartment/home.js
@@ -1,16 +1,38 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View } from '@tarojs/components'
+import { connect } from '@tarojs/redux'
+import CityModal from '@components/city-modal'
+import * as homeActions from '@actions/home'
+import * as userActions from '@actions/user'
 
+@connect(state => state, { ...homeActions, ...userActions })
 class ApartmentHome extends Component {
   config = {
     navigationBarTitleText: '公寓ABC',
     enablePullDownRefresh: true,
   }
 
+  componentDidShow() {
+    this.props.dispatchCityList()
+  }
+
+  onSelectCity(city) {
+    const citycode = city.id;
+    this.props.dispatchUserCity(citycode)
+  }
+
   render() {
+    const { user: { userInfo }, home: { cityList } } = this.props
+
     return (
       <View className='page-demo'>
-        <Text>公寓ABC</Text>
+
+        {/* 城市模态框 */}
+        <CityModal
+          citycode={userInfo.citycode}
+          cityList={cityList}
+          onSelectCity={this.onSelectCity}
+        />
       </View>
     )
   }
