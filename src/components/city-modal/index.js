dc8683b71dc085dbd43ceb795ca9a08abd2acb14
zhangxiangliang <admin@pushme.top>
2019-05-31 17:15:48 +0800

    feat: (components) 增加城市模态框

---
+++ src/components/city-modal/index.js
@@ -0,0 +1,55 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Image } from '@tarojs/components'
+import cityModelBackground from '@assets/images/home/city-modal.png'
+import { AtModal, AtModalHeader, AtModalContent } from 'taro-ui'
+
+class CityModal extends Component {
+  static options = {
+    addGlobalClass: true
+  }
+
+  static defaultProps = {
+    citycode: 0,
+    cityList: [],
+  }
+
+  render() {
+    const { citycode, cityList, onSelectCity } = this.props
+
+    const style = {
+      width: '100%',
+      height: '100%',
+      background: "#FFC919",
+    }
+
+    return (citycode === 0 &&
+      <AtModal className='city-modal' isOpened>
+
+        {/* 模态框头部 */}
+        <AtModalHeader>
+          <Image src={cityModelBackground} style={style} ></Image>
+        </AtModalHeader>
+
+        {/* 模态框内容 */}
+        <AtModalContent className='city-modal-content'>
+          <View className='city-modal-content-box'>
+            <View class='city-modal-title'>请选择当前租房位置</View>
+          </View>
+        </AtModalContent>
+
+        {/* 模态框操作 */}
+        <AtModalContent className='city-modal-action'>
+          {cityList.map(item =>
+            <View
+              className='city-modal-item'
+              key={item.id}
+              onClick={onSelectCity.bind(this, item)}
+            >{item.title}</View>
+          )}
+        </AtModalContent>
+      </AtModal>
+    )
+  }
+}
+
+export default CityModal
