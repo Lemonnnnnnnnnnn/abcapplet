0dc074ea7827adb81f2edb68f8d5155c4bb45ddf
zhangxiangliang <admin@pushme.top>
2019-06-01 20:45:59 +0800

    feat: 增加外链页面和文章页面

--- src/pages/article/show.js
+++ src/pages/article/show.js
@@ -1,16 +1,47 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View } from '@tarojs/components'
+import { connect } from '@tarojs/redux'
 
+import * as actions from '@actions/article'
+import RichTextWxParse from '@components/rich-text-wx-parse'
+import day from 'dayjs'
+
+@connect(state => state, actions)
 class ArticleShow extends Component {
   config = {
-    navigationBarTitleText: '文章详细',
+    navigationBarTitleText: '文章详情',
     enablePullDownRefresh: true,
   }
 
+  componentWillMount() {
+    const { title = '', id = 5 } = this.$router.params
+    const { articles } = this.props
+
+    // 判断文章是否存在，不存在则加载
+    const article = articles.filter(i => i.id === parseInt(id)).length > 0
+    article || this.props.dispatchArticle(parseInt(id))
+
+    // 设置标题
+    Taro.setNavigationBarTitle({ title })
+  }
+
   render() {
+    const { articles } = this.props
+    const { id = 5 } = this.$router.params
+    const article = articles.filter(i => i.id === parseInt(id))[0] || null
+    // Taro.setNavigationBarTitle({ title: article ? article.title : '' })
+
     return (
-      <View className='page-demo'>
-        <Text>文章详细</Text>
+      <View>
+        {article &&
+          <View>
+            <View className='m-2'>
+              <View className='text-huge text-bold'>{article.title}</View>
+              <View className='text-muted text-small'>{day.unix(article.create_time).format('YYYY-MM-DD')}</View>
+            </View>
+            <RichTextWxParse content={article.content} />
+          </View>
+        }
       </View>
     )
   }
---
+++ src/pages/external/index.js
@@ -0,0 +1,31 @@
+import Taro, { Component } from '@tarojs/taro'
+import { WebView } from '@tarojs/components'
+
+class ExternalIndex extends Component {
+  config = {
+    navigationBarTitleText: '访问外部链接',
+    enablePullDownRefresh: true,
+  }
+
+  state = { src: '' }
+
+  componentWillMount() {
+    const { title = '', src = '' } = this.$router.params
+
+    // 设置网页
+    this.setState({ src })
+
+    // 设置标题
+    Taro.setNavigationBarTitle({ title })
+  }
+
+  render() {
+    const { src } = this.state
+
+    return (
+      <WebView src={src}></WebView>
+    )
+  }
+}
+
+export default ExternalIndex
