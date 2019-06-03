018168bddbede368e70e921dc122b59dd5c45876
zhangxiangliang <admin@pushme.top>
2019-06-03 11:13:33 +0800

    feat: (components) 增加搜索栏固定功能

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,6 +1,7 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Picker, Text } from '@tarojs/components'
 import { AtIcon } from 'taro-ui'
+import classNames from 'classnames'
 import { MESSAGE_SEARCH_PLACEHOLDER } from "@constants/message"
 import { COLOR_GREY_0, COLOR_BLACK } from '@constants/styles'
 
@@ -14,26 +15,29 @@ class Search extends Component {
     onChangeSelector: () => { },
     selectorChecked: '',
     size: 13,
+    isFixed: false,
   }
 
   render() {
-    const { selector, onChangeSelector, selectorChecked, size } = this.props
+    const { selector, onChangeSelector, selectorChecked, size, isFixed, className } = this.props
 
     return (
-      <View class='search'>
-        <View class='search-box at-row at-row__align--center'>
-          <View class='at-col at-col-1 ml-3'>
-            <Picker mode='selector' range={selector} onChange={onChangeSelector}>
-              <View className='picker text-normal'>
-                <Text>{selectorChecked}</Text>
-                <AtIcon prefixClass='iconfont icon' value='down' size={size} color={COLOR_BLACK} />
+      <View className={classNames(className, `search ${isFixed ? 'search-fixed' : ''}`)}>
+        <View className='search-box'>
+          <View className='search-content at-row at-row__align--center'>
+            <View className='at-col at-col-1 ml-3'>
+              <Picker mode='selector' range={selector} onChange={onChangeSelector}>
+                <View className='picker text-normal'>
+                  <Text>{selectorChecked}</Text>
+                  <AtIcon prefixClass='iconfont icon' value='down' size={size} color={COLOR_BLACK} />
+                </View>
+              </Picker>
+            </View>
+            <View className='at-col'>
+              <View className='text-normal text-center text-muted'>
+                <AtIcon value='search' size={size} color={COLOR_GREY_0} />
+                <Text className='ml-2'>{MESSAGE_SEARCH_PLACEHOLDER}</Text>
               </View>
-            </Picker>
-          </View>
-          <View class='at-col'>
-            <View class='text-normal text-center text-muted'>
-              <AtIcon value='search' size={size} color={COLOR_GREY_0} />
-              <Text className='ml-2'>{MESSAGE_SEARCH_PLACEHOLDER}</Text>
             </View>
           </View>
         </View>
