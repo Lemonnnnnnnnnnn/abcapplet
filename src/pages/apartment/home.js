7889344a5a719123ea63dddb549522650f5620f7
zhangxiangliang <admin@pushme.top>
2019-06-03 11:13:53 +0800

    feat: (pages) 增加主页公寓等相关功能

--- src/pages/apartment/home.js
+++ src/pages/apartment/home.js
@@ -1,35 +1,56 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View } from '@tarojs/components'
+import { View, Text } from '@tarojs/components'
 import { connect } from '@tarojs/redux'
 
 import CityModal from '@components/city-modal'
 import Carousel from '@components/carousel'
 import Search from '@components/search'
 import Header from '@components/header'
+import Select from '@components/select'
+import Apartment from '@components/apartment'
+import Placeholder from '@components/placeholder'
+
 
 import * as adActions from '@actions/ad'
 import * as cbdActions from '@actions/cbd'
+import * as distActions from '@actions/dist'
 import * as cityActions from '@actions/city'
 import * as userActions from '@actions/user'
 import * as bannerActions from '@actions/banner'
 import * as activityActions from '@actions/activity'
 import * as recommendActions from '@actions/recommend'
+import * as apartmentActions from '@actions/apartment'
 
 import { PAGE_CBD_INDEX } from '@constants/page'
-import { MESSAGE_HOT_CBD, MESSAGE_RECOMMEND_APARTMENT, MESSAGE_ACTIVITY } from '@constants/message'
+import { MESSAGE_HOT_CBD, MESSAGE_RECOMMEND_APARTMENT, MESSAGE_ACTIVITY, MESSAGE_APARTMENT } from '@constants/message'
 
 @connect(state => state, {
   ...adActions,
   ...cbdActions,
   ...cityActions,
+  ...distActions,
   ...userActions,
   ...bannerActions,
   ...activityActions,
   ...recommendActions,
+  ...apartmentActions,
 })
 class ApartmentHome extends Component {
   config = {
     navigationBarTitleText: '公寓ABC',
+  }
+
+  state = {
+    // 搜索相关
+    searchScrollTop: null,
+    searchIsFixed: false,
+
+    // 公寓相关
+    apartmentPage: 1,
+    apartmentLoading: false,
+    apartmentHasMore: true,
+    apartmentIsFixed: false,
+    apartmentScrollTop: null,
   }
 
   componentWillMount() {
@@ -48,9 +69,18 @@ class ApartmentHome extends Component {
     this.props.dispatchUserCity(citycode)
     this.props.dispatchAdList(citycode)
     this.props.dispatchCBDList(citycode)
+    this.props.dispatchDistList(citycode)
     this.props.dispatchBannerList(citycode)
     this.props.dispatchActivityList(citycode)
     this.props.dispatchRecommendList(citycode)
+
+    // 初始化公寓相关数据
+    this.setState({
+      apartmentPage: 1,
+      apartmentHasMore: true,
+      apartmentScrollTop: null
+    })
+    this.loadingApartment()
   }
 
   /**
@@ -68,8 +98,67 @@ class ApartmentHome extends Component {
     this.onSelectCity(newCity.id)
   }
 
+  onPageScroll({ scrollTop }) {
+    const { apartmentIsFixed, apartmentScrollTop, searchScrollTop, searchIsFixed } = this.state
+
+    // 搜索相关
+    !searchScrollTop
+      && Taro.createSelectorQuery()
+        .in(this.$scope)
+        .select('.home-search')
+        .boundingClientRect()
+        .exec(res => res[0].height > 0 && this.setState({ searchScrollTop: res[0].height }))
+
+    scrollTop > searchScrollTop
+      && !searchIsFixed
+      && this.setState({ searchIsFixed: true })
+
+    scrollTop < searchScrollTop
+      && searchIsFixed
+      && this.setState({ searchIsFixed: false })
+
+    // 公寓相关
+    !apartmentScrollTop
+      && Taro.createSelectorQuery()
+        .in(this.$scope)
+        .select('.home-select')
+        .boundingClientRect()
+        .exec(res => this.setState({ apartmentScrollTop: res[0].top }))
+
+    scrollTop > apartmentScrollTop
+      && !apartmentIsFixed
+      && this.setState({ apartmentIsFixed: true })
+
+    scrollTop < apartmentScrollTop
+      && apartmentIsFixed
+      && this.setState({ apartmentIsFixed: false })
+  }
+
+  onReachBottom() {
+    this.loadingApartment()
+  }
+
+  loadingApartment() {
+    let { apartmentPage, apartmentHasMore } = this.state
+    const { user: { citycode } } = this.props
+
+    if (!apartmentHasMore) return;
+    this.setState({ apartmentLoading: true })
+
+    this.props
+      .dispatchApartmentList({
+        city: citycode,
+        current_page: apartmentPage
+      }).then(res => this.setState({
+        apartmentPage: ++apartmentPage,
+        apartmentLoading: false,
+        apartmentHasMore: res.data.data.list.length > 0
+      }))
+  }
+
   render() {
-    const { user, cbds, ads, citys, banners, recommends, activities } = this.props
+    const { apartmentLoading, apartmentIsFixed, apartmentHasMore, searchIsFixed, searchScrollTop } = this.state
+    const { user, cbds, ads, citys, banners, recommends, activities, dists, apartments } = this.props
 
     // 设置城市选择器
     const selector = citys.map(i => i.title)
@@ -78,14 +167,16 @@ class ApartmentHome extends Component {
 
     return (
       <View className='page-white m-2 mb-3'>
-        {/* <View className='page-home'> */}
-
         {/* 搜索框 & 城市选择器 */}
-        <Search
-          selector={selector}
-          selectorChecked={selectorChecked}
-          onChangeSelector={this.onChangeSelector}
-        />
+        <View className='home-search'>
+          <Search
+            className='mb-2'
+            isFixed={searchIsFixed}
+            selector={selector}
+            selectorChecked={selectorChecked}
+            onChangeSelector={this.onChangeSelector}
+          />
+        </View>
 
         {/* 轮播 */}
         {banners.length > 0 &&
@@ -104,7 +195,6 @@ class ApartmentHome extends Component {
               title={MESSAGE_HOT_CBD} url={PAGE_CBD_INDEX} hasExtra
             />
             <Carousel
-              className='mt-2'
               type='normal'
               imageHeight='176'
               imageWidth='312'
@@ -115,14 +205,16 @@ class ApartmentHome extends Component {
         }
 
         {/* 广告 */}
-        <Carousel
-          className='mt-2'
-          type='normal'
-          imageHeight='126'
-          imageWidth='686'
-          carousel={ads}
-          hasContent={false}
-        />
+        {ads.length > 0 &&
+          <Carousel
+            className='mt-2'
+            type='normal'
+            imageHeight='126'
+            imageWidth='686'
+            carousel={ads}
+            hasContent={false}
+          />
+        }
 
         {/* 推荐品牌公寓 */}
         {recommends.length > 0 &&
@@ -133,7 +225,6 @@ class ApartmentHome extends Component {
               url={PAGE_CBD_INDEX} hasExtra
             />
             <Carousel
-              className='mt-2'
               type='normal'
               imageHeight='275'
               imageWidth='642'
@@ -143,7 +234,7 @@ class ApartmentHome extends Component {
           </View>
         }
 
-        {/* 推荐品牌公寓 */}
+        {/* 活动专区 */}
         {activities.length > 0 &&
           <View>
             <Header
@@ -152,7 +243,6 @@ class ApartmentHome extends Component {
               hasExtra={false}
             />
             <Carousel
-              className='mt-2'
               type='normal'
               imageHeight='240'
               imageWidth='414'
@@ -162,13 +252,45 @@ class ApartmentHome extends Component {
           </View>
         }
 
+        {/* 严选公寓 */}
+        <View>
+          <Header
+            className='my-2'
+            title={MESSAGE_APARTMENT}
+            hasExtra={false}
+          />
+          <View className='home-select'>
+            <Select
+              top={searchScrollTop}
+              isFixed={apartmentIsFixed}
+
+              autoSortDist={[]}
+              cbdDist={dists.cbd_list}
+              priceDist={dists.price_list}
+              houseTypeDist={dists.housetype_list}
+              specialSelectDist={dists.special_select}
+            />
+          </View>
+
+          {apartments.length > 0 && apartments.map(i =>
+            <Apartment key={i.id} apartment={i} className='mt-3' />
+          )}
+          <Placeholder className='mt-2' show={apartmentLoading} quantity={5} />
+
+          {
+            !apartmentLoading && !apartmentHasMore &&
+            <View className='text-center'>
+              <Text className='text-muted'>我可是有底线的哦</Text>
+            </View>
+          }
+        </View>
+
         {/* 城市模态框 */}
         <CityModal
           city={citys}
           citycode={user.citycode}
           onSelectCity={this.onSelectCity}
         />
-        {/* </View> */}
       </View>
     )
   }
