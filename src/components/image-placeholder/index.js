382554c635b4cefc45c8594fa83b2aa71af193d5
zhangxiangliang <admin@pushme.top>
2019-06-03 11:13:08 +0800

    feat: (components) 增加 ImagePlaceholder 用于图片占位

---
+++ src/components/image-placeholder/index.js
@@ -0,0 +1,32 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View } from '@tarojs/components'
+import { COLOR_YELLOW, COLOR_WHITE } from '@constants/styles'
+
+class ImagePlaceholder extends Component {
+  static options = {
+    addGlobalClass: true
+  }
+
+  static defaultProps = {
+    height: 404,
+  }
+
+  render() {
+    const { height } = this.props
+
+    const style = {
+      width: '100%',
+      height: `${Taro.pxTransform(height)}`,
+      background: COLOR_YELLOW,
+      color: COLOR_WHITE,
+    }
+
+    return (
+      <View className='page-demo text-lager text-bold' style={style}>
+        ABC 公寓
+      </View>
+    )
+  }
+}
+
+export default ImagePlaceholder
