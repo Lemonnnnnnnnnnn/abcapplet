ca3572885380ce4c9c69fc980b66233fbe78f9b2
zhangxiangliang <admin@pushme.top>
2019-06-04 23:45:28 +0800

    feat: 增加获取下一页公寓相关的 state

--- src/actions/apartment.js
+++ src/actions/apartment.js
@@ -1,6 +1,6 @@
 import { createAction } from '@utils/redux'
 import { API_APARTMENT_LIST, PAYLOAD_APARTMENT_LIST } from '@constants/api'
-import { SET_APARTMENT_LIST } from '@constants/apartment'
+import { SET_APARTMENT_LIST, SET_NEXT_PAGE_APARTMENT_LIST } from '@constants/apartment'
 
 /**
  * 获取公寓列表
@@ -11,6 +11,17 @@ export const dispatchApartmentList = payload => createAction({
   method: 'POST',
   type: SET_APARTMENT_LIST,
   url: API_APARTMENT_LIST,
-  // TODO 格式化 url
+  cb: (res) => res.data.data.list,
+})
+
+/**
+ * 获取公寓列表
+ * @param {*} payload
+ */
+export const dispatchNextPageApartmentList = payload => createAction({
+  payload: { ...PAYLOAD_APARTMENT_LIST, ...payload },
+  method: 'POST',
+  type: SET_NEXT_PAGE_APARTMENT_LIST,
+  url: API_APARTMENT_LIST,
   cb: (res) => res.data.data.list,
 })
--- src/reducers/apartments.js
+++ src/reducers/apartments.js
@@ -1,10 +1,15 @@
 import _ from 'lodash'
-import { APARTMENT_DEFAULT, SET_APARTMENT_LIST } from '@constants/apartment'
+import { APARTMENT_DEFAULT, SET_APARTMENT_LIST, SET_NEXT_PAGE_APARTMENT_LIST } from '@constants/apartment'
 
 export default function home(state = APARTMENT_DEFAULT, action) {
   switch (action.type) {
     case SET_APARTMENT_LIST: {
-      return _.uniqWith([...state, ...action.payload], _.isEqual)
+      return _.uniqBy(action.payload, 'id')
+    }
+
+    case SET_NEXT_PAGE_APARTMENT_LIST: {
+      console.log(action.payload)
+      return _.uniqBy([...state, ...action.payload], 'id')
     }
 
     default: {
