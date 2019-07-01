4cbfdf51d94598bcc34171846a7f8ffc81972daf
zhangxiangliang <admin@pushme.top>
2019-07-01 10:15:39 +0800

    fix: 修复订单数量错误

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -1,5 +1,6 @@
 import {
   ORDER_DEFAULT,
+  SHOW_ORDER,
   SET_ORDER_LIST,
   SET_NEXT_PAGE_ORDER_LIST,
 } from '@constants/order'
@@ -14,6 +15,20 @@ export default function order(state = ORDER_DEFAULT, action) {
       const { list: oldList } = state
       const { list, total } = action.payload
       return { list: [...oldList, ...list], total }
+    }
+
+    case SHOW_ORDER: {
+      let { list } = state
+      const index = list.findIndex(i => i.id == action.payload.id)
+
+      if (index === -1) {
+        const newList = [...list, action.payload]
+        return { list: [...list, action.payload], total: newList.length }
+      } else {
+        list.splice(index, 1, { ...list[index], ...action.payload })
+        const newList = [...list]
+        return { list: [...list], total: newList.length }
+      }
     }
 
     default: {
