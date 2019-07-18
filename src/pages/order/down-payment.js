aeaa8b86ce5c89881906d73e1dd5e072130d83df
Lemon <lemon.asd99@gmail.com>
2019-07-18 20:30:41 +0800

    feat:ABC-01:缺失定金协议

--- src/pages/order/down-payment.js
+++ src/pages/order/down-payment.js
@@ -2,20 +2,16 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
 
-// 自定义组件
-import Board from '@components/board'
-import ABCIcon from '@components/abc-icon'
-import Decorate from '@components/decorate'
-
 // 自定义变量
-import { COLOR_YELLOW } from '@constants/styles'
 import {
   LOCALE_AGREEMENT_NAME,
   LOCALE_AGREEMENT_INTRODUCE,
   LOCALE_AGREEMENT_TITLE,
   LOCALE_AGREEMENT_CONTENT,
   LOCALE_DISCLAIMER,
-  LOCALE_DISCLAIMER_CONTENT
+  LOCALE_DISCLAIMER_CONTENT,
+  LOCALE_PROVACY_POLICY,
+  LOCALE_PROVACY_POLICY_CONTENT
 } from '@constants/locale'
 
 
@@ -27,11 +23,11 @@ class OrderDownPayment extends Component {
 
   render() {
     return (
-      <View className='px-3 pl-4 pr-4' >
+      <View className='mb-5 px-3 pl-4 pr-4' >
         {/* 背景底色 */}
         <View style='text-align:center' className='mt-3 mb-3 text-large text-bold '>{LOCALE_AGREEMENT_NAME}</View>
         <View style='text-indent:1.4Rem' className='text-normal'>{LOCALE_AGREEMENT_INTRODUCE}</View>
-        <View className='text-normal text-bold '>{LOCALE_AGREEMENT_TITLE}</View>
+        <View className='text-normal text-bold'  >{LOCALE_AGREEMENT_TITLE}</View>
         {
           LOCALE_AGREEMENT_CONTENT.map((value, index) => (
             <View key={index}>
@@ -39,10 +35,7 @@ class OrderDownPayment extends Component {
               <View className='text-normal '>
                 {
                   value.content.map((v, i) => (
-                    <View key={i}>
-                      <View className='text-normal'>{v.title}</View>
-                      <View className='text-normal'>{v.content}</View>
-                    </View>
+                    <View style='line-height:20px' key={i}>{v}</View>
                   ))
                 }
               </View>
@@ -57,10 +50,22 @@ class OrderDownPayment extends Component {
               <View className='text-normal '>
                 {
                   value.content.map((v, i) => (
-                    <View key={i}>
-                      <View className='text-normal'>{v.title}</View>
-                      <View className='text-normal'>{v.content}</View>
-                    </View>
+                    <View style='line-height:20px' key={i}>{v}</View>
+                  ))
+                }
+              </View>
+            </View>
+          ))
+        }
+        <View className='text-normal text-bold '>{LOCALE_PROVACY_POLICY}</View>
+        {
+          LOCALE_PROVACY_POLICY_CONTENT.map((value, index) => (
+            <View key={index}>
+              <View className='text-normal text-bold'>{value.title}</View>
+              <View className='text-normal '>
+                {
+                  value.content.map((v, i) => (
+                    <View style='line-height:20px' key={i}>{v}</View>
                   ))
                 }
               </View>
