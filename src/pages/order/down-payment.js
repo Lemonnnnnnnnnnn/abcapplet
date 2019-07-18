368e34f5212dcf46f8de17de0f2fae4be21d98fa
Lemon <lemon.asd99@gmail.com>
2019-07-18 16:07:48 +0800

    feat:ABC-20:删除原来定金协议页面的所有内容，用以存放完整协议，将协议内容定义为常量

--- src/pages/order/down-payment.js
+++ src/pages/order/down-payment.js
@@ -21,30 +21,6 @@ class OrderDownPayment extends Component {
     return (
       <View className='px-3'>
         {/* 背景底色 */}
-        <Decorate height='100' />
-
-        <Board className='p-3 mt-3'>
-          <View className='at-row at-row__justify--center'>
-            <ABCIcon icon='event_note' color={COLOR_YELLOW} size='40' />
-          </View>
-
-          <View className='at-row at-row__justify--center text-small mt-1 mb-3'>
-            {LOCALE_DOWN_PAYMENT_RULE}
-          </View>
-
-          <View className='text-normal pt-3  border-top'>
-            还没有相应接口和文本~
-          </View>
-          <View className='text-normal pt-3'>
-            只能打段话来测试测试了~
-          </View>
-          <View className='text-normal pt-3'>
-            这里是吃饭睡觉打豆豆中心~
-          </View>
-          <View className='text-normal pt-3'>
-            AV8D 举起你们的双手和我一起摇~
-          </View>
-        </Board>
       </View>
     )
   }
