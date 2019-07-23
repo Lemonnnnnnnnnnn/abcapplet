846afeffb726995c7633b0b56301dfa3804c10c0
Lemon <lemon.asd99@gmail.com>
2019-07-23 17:18:02 +0800

    fix:ABC-63:更改需求卡户型筛选

--- src/components/requirement-card-mask-house/index.js
+++ src/components/requirement-card-mask-house/index.js
@@ -2,7 +2,7 @@
 import Taro from '@tarojs/taro'
 
 import { View } from '@tarojs/components'
-import { AtTabs ,AtButton} from 'taro-ui'
+import { AtTabs, AtButton , AtTag } from 'taro-ui'
 
 
 // 自定义组件
@@ -12,23 +12,114 @@ import ABCIcon from '@components/abc-icon'
 import BaseComponent from '@components/base'
 
 // 自定义常量
-import { COLOR_GREY_2,TABS_SELECT_ITEM_HEIGHT } from '@constants/styles'
-import { LOCALE_RESET,LOCALE_CONFIRM ,LOCALE_ROOM_HOUSETYPE} from '@constants/locale'
+import { COLOR_GREY_2, TABS_SELECT_ITEM_HEIGHT } from '@constants/styles'
+import { LOCALE_RESET, LOCALE_CONFIRM, LOCALE_ROOM_HOUSETYPE } from '@constants/locale'
 
 class RequirementHouseMask extends BaseComponent {
   static defaultProps = {
     show: true,
     apartments: [],
-    headerIndex:'price',
+    headerIndex: 'price',
     priceDist: [],
 
     tabItemHeight: TABS_SELECT_ITEM_HEIGHT,
 
   }
 
+  state = {
+    room: [
+      { id: 0, title: "", active: false }
+    ],
+    floor: [
+      { id: 0, title: "", active: false }
+    ],
+    initialRoom: [],
+    initialFloor: [],
+  }
+
+  componentWillMount() {
+    setTimeout(() => {
+      const { houseDist } = this.props
+      // const { items } = this.props
+
+      const floorList = [{ id: 0, title: "不限", active: true }]
+      const roomList = [{ id: 0, title: "不限", active: true }]
+
+      houseDist.floor.map(i => {
+        i.active = false
+        floorList.push(i)
+      })
+      houseDist.room.map(i => {
+        i.active = false
+        roomList.push(i)
+      })
+
+      this.setState({
+        initialFloor: [...floorList],
+        initialRoom: [...roomList],
+        floor: [...floorList],
+        room: [...roomList],
+      })
+    }, 500)
+
+  }
+
+
+
+  onTabFloorChange(e, index) {
+    const { floor } = this.state
+    let newFloor = JSON.parse(JSON.stringify(floor))
+
+    newFloor.map(i => {
+
+      if (i.id === index) {
+        i.active = !i.active
+        const { id: type_floor } = floor[i.id]
+
+        this.props.onChange({ payload: { type_floor } })
+      } else {
+        i.active = false
+      }
+    })
+    this.setState({
+      floor: [...newFloor]
+    })
+
+  }
+
+  onTabRoomChange(e, index) {
+    const { room } = this.state
+    let newRoom = JSON.parse(JSON.stringify(room))
+
+    newRoom.map(i => {
+
+      if (i.id === index) {
+        i.active = !i.active
+        const { id: type_room } = room[i.id]
+        this.props.onChange({ payload: { type_room } })
+      } else {
+        i.active = false
+      }
+    })
+    this.setState({
+      room: [...newRoom]
+    })
+  }
+
+
+  onResetState() {
+    const { initialRoom, initialFloor } = this.state
+    this.setState({
+      room: initialRoom,
+      floor: initialFloor
+    })
+  }
+
+
   render() {
     let { show } = this.props
-    const { houseDist} = this.props
+
+    const { room, floor } = this.state
 
 
 
@@ -43,21 +134,30 @@ class RequirementHouseMask extends BaseComponent {
               <ABCIcon icon='close' color={COLOR_GREY_2} />
             </View>
           </View>
-           {/* 主体 */}
+          {/* 主体 */}
 
-           <View className='at-row at-row__justify--center'>
-
-              <AtTabs
-                scroll
-                tabDirection='vertical'
-                tabList={houseDist}
-                onClick={this.props.onChangeHouse}
-                current={this.props.current}
-              />
+          <View >
+            <View style={{ marginLeft: "10px", fontSize: "14px", fontWeight: "bold" }} >户型选择</View>
+            {
+              room.map((i, key) =>
+                <AtTag type='primary' className='ml-3 mr-2 mt-3 mb-3' circle onClick={(e) => this.onTabRoomChange(e, key)} key={key} active={i.active}>
+                  {i.title}
+                </AtTag>)
+            }
+          </View>
+          <View >
+            <View style={{ marginLeft: "10px", fontSize: "14px", fontWeight: "bold" }}>类型选择</View>
+            {
+              floor.map((i, key) =>
+                <AtTag type='primary' className='ml-3 mr-2 mt-3 mb-3' circle onClick={(e) => this.onTabFloorChange(e, key)} key={key} active={i.active}>
+                  {i.title}
+                </AtTag>)
+            }
+          </View>
 
         </View>
-          {/* 按钮 */}
-          <View>
+        {/* 按钮 */}
+        <View>
           <View className='select-button my-2 at-row at-row__justify--between'>
             <View className='at-col at-col-4'>
               <AtButton
@@ -78,13 +178,12 @@ class RequirementHouseMask extends BaseComponent {
               </AtButton>
             </View>
           </View>
-          </View>
         </View>
       </Board>
 
-      {/* 遮罩层 */}
-      <Masks show={show} style='position:relative;z-index:1'/>
-    </View>
+    {/* 遮罩层 */ }
+    <Masks show={show} style='position:relative;z-index:1' />
+    </View >
   }
 }
 
