dd98a1ecc8e75ffd02be700038b61bd0630c4253
Lemon <lemon.asd99@gmail.com>
2019-07-29 14:02:34 +0800

    feat:ABC-88: 需求卡弹窗时底部被遮盖层级仍可滑动，应禁止

--- src/components/appointment-post-mask/index.js
+++ src/components/appointment-post-mask/index.js
@@ -1,7 +1,7 @@
 // Taro 相关
 import Taro from '@tarojs/taro'
 import { View, Input } from '@tarojs/components'
-import { AtButton} from 'taro-ui'
+import { AtButton } from 'taro-ui'
 
 
 // 自定义组件
@@ -20,10 +20,14 @@ class AppointPostMask extends BaseComponent {
     apartments: [],
   }
 
-  render() {
-    let { show ,name , tel} = this.props
+  onMaskTouchMove(e) {
+    return e.stopPropagation()
+  }
 
-    return show && <View className='apartment-mask'>
+  render() {
+    let { show, name, tel } = this.props
+
+    return show && <View className='apartment-mask' onTouchMove={this.onMaskTouchMove}>
       {/* 主体内容 */}
       <Board fixed='bottom' border='top'>
         <View className='m-3'>
@@ -38,31 +42,31 @@ class AppointPostMask extends BaseComponent {
             <View className='at-col-3 text-normal at-row at-row__align--center '>姓名：</View>
             <View className='at-col-8 p-1 at-row ' style='background:#F8F8F8; border-radius: 30px'>
               {
-                name==='姓名' ? 
-                <Input className='text-normal ml-2'   placeholder='修改姓名' onChange={this.props.onGetName} ></Input>
-                : 
-                <Input className='text-normal ml-2'   placeholder='修改姓名' onChange={this.props.onGetName}value={name} ></Input> 
+                name === '姓名' ?
+                  <Input className='text-normal ml-2' placeholder='修改姓名' onChange={this.props.onGetName} ></Input>
+                  :
+                  <Input className='text-normal ml-2' placeholder='修改姓名' onChange={this.props.onGetName} value={name} ></Input>
               }
-              
+
             </View>
           </View>
           <View className='at-row mt-3'>
             <View className='at-col-3 text-normal at-row at-row__align--center '>电话号码：</View>
             <View className='at-col-8 p-1 at-row ' style='background:#F8F8F8; border-radius: 30px'>
-            {
-                tel==='电话' ? 
-                <Input className='text-normal ml-2 '  placeholder='修改电话号码' onChange={this.props.onGetTel} ></Input>
-                : 
-                <Input className='text-normal ml-2 '  placeholder='修改电话号码' onChange={this.props.onGetTel} value={tel}></Input>
+              {
+                tel === '电话' ?
+                  <Input className='text-normal ml-2 ' placeholder='修改电话号码' onChange={this.props.onGetTel} ></Input>
+                  :
+                  <Input className='text-normal ml-2 ' placeholder='修改电话号码' onChange={this.props.onGetTel} value={tel}></Input>
               }
-              
+
             </View>
           </View>
           <AtButton
             circle
             className='mt-3 mb-2 btn-yellow active'
             onClick={this.props.onClose}
-            >确定</AtButton>
+          >确定</AtButton>
         </View>
       </Board>
 
--- src/components/service-evaluation-mask/index.js
+++ src/components/service-evaluation-mask/index.js
@@ -1,7 +1,7 @@
 // Taro 相关
 import Taro from '@tarojs/taro'
 import { View } from '@tarojs/components'
-import { AtIcon ,AtTextarea, AtButton } from 'taro-ui'
+import { AtIcon, AtTextarea, AtButton } from 'taro-ui'
 
 
 // 自定义组件
@@ -11,9 +11,9 @@ import ABCIcon from '@components/abc-icon'
 import Component from '@components/base'
 
 // 自定义常量
-import { COLOR_YELLOW , COLOR_GREY_7 ,COLOR_GREY_2} from '@constants/styles'
+import { COLOR_YELLOW, COLOR_GREY_7, COLOR_GREY_2 } from '@constants/styles'
 import { BUSINESS_LIKEABILITY_DIST } from '@constants/appointment'
-import { PAYLOAD_REVULUTION_CREAT} from '@constants/api'
+import { PAYLOAD_REVULUTION_CREAT } from '@constants/api'
 import { connect } from '@tarojs/redux'
 import * as appointmentActions from '@actions/appointment'
 
@@ -22,48 +22,52 @@ import {
   LOCALE_APPOINTMENT_SERVICE_EVALUTION
 } from '@constants/locale'
 
-@connect(state => state , {
+@connect(state => state, {
   ...appointmentActions,
 })
 
 class AppointmentRevolution extends Component {
   static defaultProps = {
     show: false,
-    Payload:PAYLOAD_REVULUTION_CREAT,
+    Payload: PAYLOAD_REVULUTION_CREAT,
 
-    score:'',
-    lists:''
-}
+    score: '',
+    lists: ''
+  }
 
-//分数，星星数
-onScoreChange(score) {
-  const { payload } = this.state
-  this.setState({
-    score:score,
-    payload: { ...payload, score:score ,appointment_id:this.props.appointment_id}
-  })
+  //分数，星星数
+  onScoreChange(score) {
+    const { payload } = this.state
+    this.setState({
+      score: score,
+      payload: { ...payload, score: score, appointment_id: this.props.appointment_id }
+    })
 
-}
-//获得评论
-onRemarkChange({ currentTarget: { value } }){
-  let { payload } = this.state
-  this.setState({ payload: { ...payload, comment: value ,appointment_id:this.props.appointment_id} })
-}
-//提交评价
-onClickPost(){
-  let { payload } = this.state
-  this.props.dispatchRevelutionComment(payload)
-}
+  }
+  //获得评论
+  onRemarkChange({ currentTarget: { value } }) {
+    let { payload } = this.state
+    this.setState({ payload: { ...payload, comment: value, appointment_id: this.props.appointment_id } })
+  }
+  //提交评价
+  onClickPost() {
+    let { payload } = this.state
+    this.props.dispatchRevelutionComment(payload)
+  }
+
+  onMaskTouchMove(e) {
+    return e.stopPropagation()
+  }
 
   render() {
-    const { show , headimgurl,name,service_num,comment_score,appointment_id ,comment} = this.props
+    const { show, headimgurl, name, service_num, comment_score, appointment_id, comment } = this.props
     const { score } = this.state
     var nowScore;
-    nowScore =  comment !== undefined ? (
-        comment.score !== undefined && comment.score === 0 ? score : comment.score
-       ) : score;
+    nowScore = comment !== undefined ? (
+      comment.score !== undefined && comment.score === 0 ? score : comment.score
+    ) : score;
 
-    return show && <View className='apartment-mask'>
+    return show && <View className='apartment-mask' onTouchMove={this.onMaskTouchMove}>
       {/* 主体内容 */}
       <Board fixed='bottom' border='top'>
         <View className='m-3'>
@@ -94,8 +98,8 @@ onClickPost(){
           <View className='at-row mt-3 ml-2  at-row__align--end'>
             <View className='text-large text-bold'>{LOCALE_APPOINTMENT_SERVICE_EVALUTION} </View>
             <View className='text-large ml-3' style='color:#FFCB1F' >
-               {BUSINESS_LIKEABILITY_DIST[nowScore].message}
-             </View>
+              {BUSINESS_LIKEABILITY_DIST[nowScore].message}
+            </View>
 
           </View>
           <View className='mt-4' >
@@ -105,8 +109,8 @@ onClickPost(){
                 key={i}
                 value='star-2'
                 className='ml-3'
-                onClick={this.onScoreChange.bind(this,i)}
-                color={i<=nowScore?COLOR_YELLOW:COLOR_GREY_7}
+                onClick={this.onScoreChange.bind(this, i)}
+                color={i <= nowScore ? COLOR_YELLOW : COLOR_GREY_7}
               />
             )}
           </View>
@@ -125,12 +129,12 @@ onClickPost(){
             />
           </View>
           {/* 按钮 */}
-          <View className='mt-4' hidden={comment.score ===0 ? false:true}>
+          <View className='mt-4' hidden={comment.score === 0 ? false : true}>
             <AtButton
-              onClick={this.onClickPost.bind(this,appointment_id)}
+              onClick={this.onClickPost.bind(this, appointment_id)}
               circle
               type='primary'
-              >匿名评价</AtButton>
+            >匿名评价</AtButton>
 
           </View>
         </View>
