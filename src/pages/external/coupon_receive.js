0d81ca933c151dabf6575fddd8061f8f5bb7e678
Lemon <lemon.asd99@gmail.com>
2019-10-24 17:32:18 +0800

    feat:新增优惠券领取页面中...

--- src/pages/external/coupon_receive.js
+++ src/pages/external/coupon_receive.js
@@ -3,14 +3,16 @@ import { View } from '@tarojs/components'
 import ApartmentCouponList from '@components/apartment-coupon-list'
 import { PAYLOAD_COUPON_LIST } from '@constants/api'
 
+// 自定义组件
 import BaseComponent from '@components/base'
+import Board from '@components/board'
 
 // redux相关
 import { connect } from '@tarojs/redux'
 import * as apartmentActions from '@actions/apartment'
 
+import '../../styles/_page.scss'
 import '../../styles/_board.scss'
-
 
 @connect(state => state, {
   ...apartmentActions,
@@ -30,23 +32,30 @@ class ExternalIndex extends BaseComponent {
   render() {
     const { apartmentCouponList: { list } } = this.props
     const { id } = this.$router.params
-    return (
-      <View >
-        <View className='decorate-gradient'></View>
-        {/* title */}
-        <View className='text-huge text-bold ml-3 mt-3'>待领取优惠券</View>
-        {/* list */}
-        <ApartmentCouponList
-          block='apartment'
-          couponList={list}
-          ref={this.CouponList}
-          params={this.props.params}
-          onListRefresh={this.onListRefresh}
 
-          defaultPayload={{ ...PAYLOAD_COUPON_LIST, apartment_id: id }}
-          dispatchList={this.props.dispatchCouponListPost}
-          dispatchNextPageList={this.props.dispatchNextPageCouponListPost}
-        />
+    return (
+      <View className='page-orange'>
+        {/* 白色纸条 */}
+
+        <View className='vertical-level-center board--all board--wrap board'>
+
+          <View className='p-3'>
+            {/* 金额 */}
+            <View className='at-row  at-row__justify--center  board--all board--pink text-bold' >
+              <View style={{ fontSize: Taro.pxTransform(31 * 2) }}>￥</View>
+              <View style={{ fontSize: Taro.pxTransform(70 * 2) }}>500</View>
+            </View>
+            {/* 分隔符 */}
+            <View className='line-dotted mt-3 mb-3'></View>
+            {/* 条件 */}
+            <View>
+              <View>全场通用券</View>
+              <View className='text-secondary'>优惠券使用条件</View>
+              <View></View>
+            </View>
+          </View>
+
+        </View>
 
       </View>
     )
